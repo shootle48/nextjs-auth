8673cdf510c7c6c3bb5d1dfbce44c9d31095c64d
shootle48 <theprit.1316s@gmail.com>
2024-10-24 22:22:12 +0700

    commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,16 @@
-"use client"
+"use client";
 
 import Navbar from "./components/Navbar";
-import Main from "./components/Main"
+import Main from "./components/Main";
 import { useSession } from "next-auth/react";
 
 export default function Home() {
-
-  const { data: session } = useSession()
+  const { data: session } = useSession();
 
   return (
     <main>
-      <Navbar session={session}/>
-      <Main/>
+      <Navbar session={session} />
+      <Main />
     </main>
   );
 }
